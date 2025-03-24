ddbe9271ae982957a32ba72ae4d552b0c6d399ca
jmogielnicki <jmogielnicki@gmail.com>
2025-03-24 14:42:24 +0000

    vite config - base url for custom domain

--- web/vite.config.js
+++ web/vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	base: "/parlourbench/", // Make sure this matches your repo name
+	base: "/", // Make sure this matches your repo name
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
